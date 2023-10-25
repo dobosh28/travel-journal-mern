35d541703a6548275de6223d9e7e98dd96b41e11
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-10-25 00:40:28 -0400

    add CORS

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
+const cors = require("cors");
+const csurf = require("csurf");
+const { isProduction } = require("./config/keys");
+
 const usersRouter = require("./routes/api/users");
 const journalsRouter = require("./routes/api/journals");
 
@@ -13,9 +15,28 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, the Express
+  // server will serve the React files statically.)
+  app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+// Express Routes
 app.use("/api/users", usersRouter);
 app.use("/api/journals", journalsRouter);
 
