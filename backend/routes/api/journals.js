1590d8e1b1a6c213720d5fab78d12e33b33a7a45
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-10-25 00:15:31 -0400

    remove public folder and index.js file

--- backend/routes/api/journals.js
+++ backend/routes/api/journals.js
@@ -1,62 +1,62 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
   res.json({
     message: "POST /api/journals",
   });
 });
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   res.json({
     message: "GET /api/journals",
   });
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
   res.json({
     message: "GET /api/journals/:id",
   });
 });
 
-router.patch("/:id", (req, res) => {
+router.patch("/:id", (req, res, next) => {
   res.json({
     message: "PATCH /api/journals/:id",
   });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res, next) => {
   res.json({
     message: "DELETE /api/journals/:id",
   });
 });
 
 // Comments for journal entries
-router.post("/:id/comments", (req, res) => {
+router.post("/:id/comments", (req, res, next) => {
   res.json({
     message: "POST /api/journals/:id/comments",
   });
 });
 
-router.get("/:id/comments", (req, res) => {
+router.get("/:id/comments", (req, res, next) => {
   res.json({
     message: "GET /api/journals/:id/comments",
   });
 });
 
-router.get("/:id/comments/:comment_id", (req, res) => {
+router.get("/:id/comments/:comment_id", (req, res, next) => {
   res.json({
     message: "GET /api/journals/:id/comments/:comment_id",
   });
 });
 
-router.patch("/:id/comments/:comment_id", (req, res) => {
+router.patch("/:id/comments/:comment_id", (req, res, next) => {
   res.json({
     message: "PATCH /api/journals/:id/comments/:comment_id",
   });
 });
 
-router.delete("/:id/comments/:comment_id", (req, res) => {
+router.delete("/:id/comments/:comment_id", (req, res, next) => {
   res.json({
     message: "DELETE /api/journals/:id/comments/:comment_id",
   });
