bc8af245f42058fc5643572cecfe77ce240f13b1
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-11-12 20:51:48 -0500

    edit and fix register validations

--- backend/validations/register.js
+++ backend/validations/register.js
@@ -1,23 +1,30 @@
 const { check } = require("express-validator");
-const handleValidationErrors = require('./handleValidationErrors');
+const handleValidationErrors = require("./handleValidationErrors");
 
-// validateRegisterInput is a combination Express middleware that uses the 
-// `check` middleware to validate the keys in the body of a request to 
+// Regex patterns from the Mongoose model
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+const usernameRegex = /^[a-zA-Z0-9_]+$/;
+const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+
+// validateRegisterInput is a combination Express middleware that uses the
+// `check` middleware to validate the keys in the body of a request to
 // register a user
 const validateRegisterInput = [
-  check('email')
+  check("email")
     .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage('Email is invalid'),
-  check('username')
+    .matches(emailRegex)
+    .withMessage("Email is invalid"),
+  check("username")
     .exists({ checkFalsy: true })
-    .isLength({ min: 2, max: 30 })
-    .withMessage('Username must be between 2 and 30 characters'),
-  check('password')
+    .matches(usernameRegex)
+    .withMessage("Username is invalid"),
+  check("password")
     .exists({ checkFalsy: true })
-    .isLength({ min: 6, max: 30 })
-    .withMessage('Password must be between 6 and 30 characters'),
-  handleValidationErrors
+    .matches(passwordRegex)
+    .withMessage(
+      "Password must be at least 8 characters, include at least one number and one special character"
+    ),
+  handleValidationErrors,
 ];
 
 module.exports = validateRegisterInput;
