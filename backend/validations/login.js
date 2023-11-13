6968d231f56a299bcd53474bd556993afa800baf
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-11-12 20:38:12 -0500

    fix login validation

--- backend/validations/login.js
+++ backend/validations/login.js
@@ -1,13 +1,22 @@
-const { check } = require("express-validator");
+const { check, oneOf } = require("express-validator");
 const handleValidationErrors = require("./handleValidationErrors");
 
 // validateLoginInput is a combination Express middleware that uses the `check`
 // middleware to validate the keys in the body of a request to login a user
 const validateLoginInput = [
-  check("email")
-    .exists({ checkFalsy: true })
-    .isEmail()
-    .withMessage("Email is invalid"),
+  oneOf(
+    [
+      check("email")
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage("Email is invalid"),
+      check("username")
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2, max: 30 })
+        .withMessage("Username must be between 2 and 30 characters"),
+    ],
+    "Must provide either an email address or username"
+  ),
   check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6, max: 30 })
