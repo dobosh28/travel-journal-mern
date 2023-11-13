a29018a0941be2aa7fbca5fafdf5715848fc468d
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-11-12 20:48:04 -0500

    update login validation

--- backend/validations/login.js
+++ backend/validations/login.js
@@ -1,6 +1,11 @@
 const { check, oneOf } = require("express-validator");
 const handleValidationErrors = require("./handleValidationErrors");
 
+// Regex patterns from the Mongoose model
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+const usernameRegex = /^[a-zA-Z0-9_]+$/;
+const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+
 // validateLoginInput is a combination Express middleware that uses the `check`
 // middleware to validate the keys in the body of a request to login a user
 const validateLoginInput = [
@@ -8,19 +13,21 @@ const validateLoginInput = [
     [
       check("email")
         .exists({ checkFalsy: true })
-        .isEmail()
+        .matches(emailRegex)
         .withMessage("Email is invalid"),
       check("username")
         .exists({ checkFalsy: true })
-        .isLength({ min: 2, max: 30 })
-        .withMessage("Username must be between 2 and 30 characters"),
+        .matches(usernameRegex)
+        .withMessage("Username is invalid"),
     ],
     "Must provide either an email address or username"
   ),
   check("password")
     .exists({ checkFalsy: true })
-    .isLength({ min: 6, max: 30 })
-    .withMessage("Password must be between 6 and 30 characters"),
+    .matches(passwordRegex)
+    .withMessage(
+      "Password must be at least 8 characters, include at least one number and one special character"
+    ),
   handleValidationErrors,
 ];
 
