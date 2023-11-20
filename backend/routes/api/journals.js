adf591cab5799ffb23f31b00bf959f9164d0a596
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-11-19 23:41:30 -0500

    make journal route edits

--- backend/routes/api/journals.js
+++ backend/routes/api/journals.js
@@ -1,18 +1,57 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const Journal = mongoose.model("Journal");
+const { requireUser } = require("../../config/passport");
+const validateJournalInput = require("../../validations/journals");
 
 // GET all journals
-router.get("/", (req, res, next) => {
-  res.json({
-    message: "GET all journals",
-  });
+router.get("/", async (req, res) => {
+  try {
+    const journals = await Journal.find()
+      .populate("author", "_id username")
+      .sort({ createdAt: -1 });
+    return res.json(journals);
+  } catch (err) {
+    return res.json([]);
+  }
+});
+
+router.get("/user/:userId", async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.params.userId);
+  } catch (err) {
+    const error = new Error("User not found");
+    error.statusCode = 404;
+    error.errors = { message: "No user found with that id" };
+    return next(error);
+  }
+  try {
+    const tweets = await Tweet.find({ author: user._id })
+      .sort({ createdAt: -1 })
+      .populate("author", "_id username");
+    return res.json(tweets);
+  } catch (err) {
+    return res.json([]);
+  }
 });
 
 // GET a single journal
-router.get("/:id", (req, res, next) => {
-  res.json({
-    message: "GET a single journal",
-  });
+router.get("/:id", async (req, res, next) => {
+  try {
+    const journal = await Journal.findById(req.params.id).populate(
+      "author",
+      "_id username"
+    );
+    return res.json(journal);
+  } catch (err) {
+    const error = new Error("Journal not found");
+    error.statusCode = 404;
+    error.errors = { message: "No journal found with that id" };
+    return next(error);
+  }
 });
 
 // POST a new journal
@@ -68,4 +107,3 @@ router.delete("/:id/comments/:comment_id", (req, res, next) => {
 });
 
 module.exports = router;
-
